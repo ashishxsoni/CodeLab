fa5fd3cf1aff209137a1a5b0d878e0af3e899a3b
ashishxsoni <ashishsonich12@gmail.com>
2025-06-19 18:27:20 +0530

    fix the error

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -11,7 +11,7 @@ import ChatMessages from "./ChatMessages";
 import ControlBar from "./ControlBar";
 import ControlBarButton from "./ControlBarButton";
 import EscNotification from "./EscNotification";
-import Sidebar from "./SideBar";
+import Sidebar from "./Sidebar";
 import ShareLinkPopup from "./ShareLinkPopup";
 import LeaveRoomComponent from "./LeaveRoom";
 export { CodeEditor, Footer, Header, ParticipantsList, ProfilePanel, HomeContent, ThemeButton , LoadingScreen, AccessDeniedScreen , 
